3a945008f73521220a854a089da79c7f6bbfdae5
Friedrich Völkers <friedrich.voelkers@me.com>
2021-11-20 14:44:51 +0100

    Update for beta publish

---
+++ dist/precache-manifest.3b980be79ba26938bb1678262a36395b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee24d839d9b6e9dc329",
+    "url": "/css/app.ea327113.css"
+  },
+  {
+    "revision": "afc48989b9f017e00e7a",
+    "url": "/css/chunk-vendors.0d9171ac.css"
+  },
+  {
+    "revision": "4849ceaa913b66a3d8fa049acfcbbb17",
+    "url": "/img/dc_1.4849ceaa.png"
+  },
+  {
+    "revision": "f9acdfbdb49b9211265c4a517e29dab9",
+    "url": "/img/pf_2.f9acdfbd.gif"
+  },
+  {
+    "revision": "76ce3a0bc076196e2aab193033afd77d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd1c45a17d7ecc6fd8e7",
+    "url": "/js/about.a7e39230.js"
+  },
+  {
+    "revision": "0ee24d839d9b6e9dc329",
+    "url": "/js/app.66eed87c.js"
+  },
+  {
+    "revision": "afc48989b9f017e00e7a",
+    "url": "/js/chunk-vendors.ad2cb82f.js"
+  },
+  {
+    "revision": "db31bf4807f47c668280a84a26a5bfcf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
